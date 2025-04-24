0fb67471f92c6ae0c838b61b3ef52975baaccea0
VLorik <mail2vlorik@gmail.com>
2025-04-24 14:52:09 +0530

    refine the code

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -4,6 +4,7 @@ import { cookies } from "next/headers";
 export async function createClient() {
   const cookieStore = await cookies();
 
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
